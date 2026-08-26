6a659fa594056353fc962168176125fced13843d
felimomo <felimomouni@gmail.com>
2026-08-26 15:11:01 -0600

    debug, incluir resultados.

--- 01-FFT-PDE/burger.py
+++ 01-FFT-PDE/burger.py
@@ -1,39 +1,103 @@
+import argparse
+
 import numpy as np
 from scipy.integrate import solve_ivp
 import scipy.fft
-import scipy.fft.fft as FFT
-import scipy.fft.ifft as IFFT
+from scipy.fft import rfft as FFT
+from scipy.fft import dst as DST
+from scipy.fft import irfft as IFFT
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+	'-hm', '--homogenea', 
+	action='store_true', default=False, 
+	help='Incluir este argumento si la ec es homogenea.'
+)
+args = parser.parse_args()
+
+# versión de la ecuación
+HOMOGENEA = args.homogenea
 
 # discretización
-N = 128
+N = (2 ** 8)
 
 # condiciones iniciales
-f0 = np.zeros(N)
-t_eval = [0.1 * t for t in range(11)]
-k = scipy.fft.fftfreq(N)
-X = np.linspace(0, 1, N)
+T = 10
+N_t = 10
+t_eval = [(T / N_t) * t for t in range(N_t+1)]
+X = np.linspace(0, 2 * np.pi, N)
+k = scipy.fft.rfftfreq(N, 1./N)
 
-def S(x,t):
-	st = np.cos(2 * np.pi * t)
-	return np.exp(-(x - st) ** 2 / 2)
+print('Frecuencias k usadas:')
+print(k)
 
-def G(t, u: np.ndarray):
-	sk = FFT(np.array([S(x, t) for x in X]))
-	return (
+f0 = FFT(np.sin(X))
+VISC = 0.1
+
+def G(t, u: np.ndarray, homogenea: bool = False):
+	if homogenea:
+		sk=0
+	else:
+		st = 0.2 * np.sin(2 * 10 * np.pi * t)
+		sk = (
+			5 * np.sqrt(np.pi)
+			* np.exp(1j * k * (np.pi+st))
+			* np.exp(
+				- np.pi ** 2
+				* k ** 2
+			)
+		)
+
+	r = (
 		- FFT(
 			IFFT(u) * IFFT(1j * k * u)
 		)
-		- (k ** 2) * u
+		- VISC * (k ** 2) * u
 		+ sk
 	)
+	# print(f't={t:.2f}, | sk | / | r | = {np.linalg.norm(sk)/np.linalg.norm(r):.2f}', end='\r')
+	return r
 
 def main():
-	fourier_result = solve_ivp(fun = G, t_span = [0,1], y0=f0, t_eval=t_eval)
+	fourier_result = solve_ivp(
+		fun = G, 
+		t_span = [t_eval[0],t_eval[-1]], 
+		y0=f0, 
+		t_eval=t_eval,
+		args=[HOMOGENEA]
+	)
 	f = np.array(
 		[
-			IFFT(fourier_f for fourier_f in fourier_result.y)
+			IFFT(fourier_f)
+			for fourier_f in fourier_result.y.T
 		]
 	)
 
-	# plottear soluciones para cada t:
-	...
+	df = pd.DataFrame({ 
+		**{
+			f't_{t}': f[i]
+			for i, t in enumerate(t_eval)
+		},
+		**{
+			'x': [2 * np.pi * l/len(f[0]) for l in range(len(f[0]))]
+		}
+		})
+
+	axes = df.plot(x='x')
+	plt.legend(
+		bbox_to_anchor=(1.05, 0.5), 
+		loc='center left'
+	)
+	plt.tight_layout()
+
+	if HOMOGENEA:
+		plt.savefig('homogenea.png')
+	else:
+		plt.savefig('fuente.png')
+
+
+if __name__=='__main__':
+	main()
